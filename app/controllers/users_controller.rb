004ab4ebfb172429efe2a545043382787763c16a
kohtomoda <info@oj9.co.jp>
2016-04-18 04:24:03 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 before_action :logged_in_user, only: [:edit, :update]
 before_action :correct_user,   only: [:edit, :update]
 
+
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews.order(created_at: :desc)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331081922) do
+ActiveRecord::Schema.define(version: 20160412103507) do
 
   create_table "movie_images", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20160331081922) do
   end
 
   add_index "movies", ["user_id"], name: "index_movies_on_user_id"
+
+  create_table "moviethreads", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
